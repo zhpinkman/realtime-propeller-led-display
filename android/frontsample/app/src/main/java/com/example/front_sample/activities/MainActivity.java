8bc20252a56563d9b36abf154b7b6db8b54278f6
zhivar <zhivarsourati@gmail.com>
2020-06-03 20:25:25 +0430

    Add VideoActivity

--- android/frontsample/app/src/main/java/com/example/front_sample/activities/MainActivity.java
+++ android/frontsample/app/src/main/java/com/example/front_sample/activities/MainActivity.java
@@ -36,4 +36,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onVideoActivity(View view) {
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
+    }
+
 }
