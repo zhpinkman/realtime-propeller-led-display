036282758225572105c3f81ae96629c7b94736c5
zhivar <zhivarsourati@gmail.com>
2020-06-03 21:51:09 +0430

    Add simple video presentation

--- android/frontsample/app/src/main/java/com/example/front_sample/activities/VideoActivity.java
+++ android/frontsample/app/src/main/java/com/example/front_sample/activities/VideoActivity.java
@@ -1,19 +1,79 @@
 package com.example.front_sample.activities;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.front_sample.R;
 
+import java.io.File;
+
+
 public class VideoActivity extends AppCompatActivity {
+
+    private static final int VIDEO_GALLERY_REQUEST = 30;
+    private VideoView videoView;
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+
+        init();
+
+        playVideoRawFolder();
+
+        setMediaCont(); 
+
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 
 
-    
+    public void onSend(View view) {
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+
+        photoPickerIntent.setDataAndType(data, "video/*");
+
+        startActivityForResult(photoPickerIntent, VIDEO_GALLERY_REQUEST);
+
+    }
+
+    public void init() {
+        videoView = findViewById(R.id.videoView3);
+        this.mediaController = new MediaController(this);
+    }
+
+
+    private void playVideoRawFolder() {
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video1);
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+
+    private void setMediaCont() {
+        mediaController.setMediaPlayer(videoView);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+    }
 }
