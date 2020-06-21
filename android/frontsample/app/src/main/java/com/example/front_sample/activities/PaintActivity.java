886f33126fc18b573810cb76c7d440228e18e851
Ali Keramatipour <alikeramatipour@Alis-MacBook-Pro-2.local>
2020-06-21 23:42:20 +0430

    added PaintView

--- android/frontsample/app/src/main/java/com/example/front_sample/activities/PaintActivity.java
+++ android/frontsample/app/src/main/java/com/example/front_sample/activities/PaintActivity.java
@@ -3,16 +3,56 @@ package com.example.front_sample.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.front_sample.R;
+import com.example.front_sample.utils.PaintView;
+
 
 public class PaintActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+    private int finalWidth = 256;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
-        ((TextView) findViewById(R.id.textView2)).setText("paint mode");
+        paintView = (PaintView) findViewById(R.id.myPaintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        metrics.heightPixels = metrics.widthPixels;
+        int customLen = 0;
+        while (customLen <= metrics.widthPixels)
+            customLen += finalWidth;
+        customLen -= finalWidth;
+        metrics.heightPixels = customLen;
+        metrics.widthPixels = customLen;
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.saveImage:
+                paintView.saveImage();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
